a5a9be8d25425a6f8325147ef2a77189a1723958
vani23du <vani23du@student.ju.se>
2025-10-08 14:34:44 +0200

    Worked on the background and added field code.

--- ocean.js
+++ ocean.js
@@ -1,7 +1,175 @@
+//REFERENCES
+//https://codepen.io/pixelkind/pen/JjwGQgd - the following code was created with the help of this tutorial
+//https://chatgpt.com/share/68e569bc-9108-8003-be50-cb73fb308bed - the following code was created with the help of chatGPT
+
+//VARIABLES
+let drawX = 0;
+let drawY = 0;
+
+/*-------------------------------*/
+
+//SETUP FUNCTIONS
 function setup() {
-    createCanvas(innerWidth, innerHeight);
+  /*creating canvas with system variables*/
+  createCanvas(windowWidth, windowHeight);
+  noLoop();
 }
 
+function windowResized() {
+  //resize p5.js canvas to fit window width and height
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+/*-------------------------------*/
+//OTHER FUNCTIONS
+function canvasBase(/*creating the back of the background*/) {
+  //main area of the artwork and the background colour of it
+  push();
+  fill(0, 0, 0);
+  rectMode(CENTER);
+  rect(width / 2, height / 2, width, 600);
+  pop();
+}
+
+function drawWaterVariation() {
+  push();
+  const waterVariationFields = 70;
+  const waterHeight = 600;
+  const variationWidth = width / waterVariationFields;
+  const variationHeight = waterHeight / waterVariationFields;
+  noStroke();
+  for (let xVariation = 0; xVariation < waterVariationFields; xVariation++) {
+    for (let yVariation = 0; yVariation < waterVariationFields; yVariation++) {
+      if (Math.random() < 0.002) {
+        fill(255, 255, 255);
+      } else {
+        noFill();
+      }
+      square(
+        xVariation * variationWidth,
+        yVariation * variationHeight,
+        variationHeight
+      );
+    }
+  }
+  pop();
+}
+
+//OTHER THINGS (CANVES, BOATS(?) ETC.)
+function drawCave() {
+  push();
+  fill(0, 0, 0);
+  rect(drawX + 710, drawY + 500, 100, 100);
+  rect(drawX + 770, drawY + 400, 60, 120);
+  rect(drawX + 800, drawY + 480, 80, 120);
+  pop();
+}
+
+/*-------------------------------*/
+//CLIFF FUNCTIONS
+function drawCliff1() {
+  push();
+  noStroke();
+  fill(0, 0, 30);
+  //basics
+  rect(drawX, drawY + 200, 100, 400);
+  rect(drawX + 100, drawY + 300, 100, 300);
+  rect(drawX + 200, drawY + 400, 100, 200);
+  rect(drawX + 300, drawY + 500, 100, 100);
+  //cliff extra pixels
+  rect(drawX + 100, drawY + 250, 30, 100);
+  rect(drawX + 300, drawY + 450, 40, 100);
+  pop();
+}
+
+function drawCliff2() {
+  push();
+  noStroke();
+  fill(0, 0, 30);
+  //basics
+  rect(drawX + 500, drawY + 500, 100, 100);
+  rect(drawX + 600, drawY + 400, 100, 200);
+  rect(drawX + 650, drawY + 300, 300, 300);
+  rect(drawX + 800, drawY + 250, 100, 200);
+  rect(drawX + 900, drawY + 500, 100, 100);
+
+  //cliff extra pixels
+  rect(drawX + 1000, drawY + 550, 20, 50);
+  pop();
+}
+
+/*-------------------------------*/
+//PLANTATION VARIATION - Gonna draw more
+function drawPlantType1() {
+  push();
+  noStroke();
+  fill(23, 40, 23);
+  //stem
+  rect(drawX + 115, drawY + 230, 7, 20);
+  //left thing
+  rect(drawX + 110, drawY + 215, 7, 17);
+  rect(drawX + 106, drawY + 200, 5, 20);
+  rect(drawX + 108, drawY + 180, 3, 20);
+  //middle thing
+  rect(drawX + 115, drawY + 190, 5, 40);
+  rect(drawX + 118, drawY + 172, 3, 20);
+  //right thing
+  rect(drawX + 120, drawY + 220, 6, 14);
+  rect(drawX + 125, drawY + 200, 6, 25);
+  rect(drawX + 130, drawY + 190, 3, 25);
+  pop();
+}
+
+function drawPlantType2() {
+  push();
+  noStroke();
+  fill(23, 50, 23);
+  rect(drawX + 20, drawY + 570, 10, 30);
+  rect(drawX + 30, drawY + 550, 5, 50);
+  rect(drawX + 35, drawY + 560, 5, 40);
+  rect(drawX + 40, drawY + 530, 5, 70);
+  rect(drawX + 45, drawY + 540, 7, 60);
+  rect(drawX + 45, drawY + 540, 7, 60);
+  rect(drawX + 50, drawY + 580, 15, 20);
+  rect(drawX + 60, drawY + 530, 5, 60);
+  rect(drawX + 65, drawY + 550, 5, 50);
+  pop();
+}
+
+/*-------------------------------*/
+
+//!THE! DRAW FUNCTION
 function draw() {
-    rect(); //Add for the sea area
+  background(255, 255, 255);
+  canvasBase();
+  let variationFit = height / 2 - 300;
+  translate(0, variationFit);
+
+  //DRAWING THE CLIFFS
+  drawCliff1();
+  drawCliff2();
+  push(/*draw extra cliff by the edge*/);
+  translate(drawX + 700, drawY);
+  drawCliff2();
+  pop();
+
+  //DRAW THE EXTRA STUFF
+  drawCave();
+
+  //DRAWING THE PLANTS
+  //plant type 1 - placement 1
+  drawPlantType1();
+  push(/*plant type 1 - placement 2 (by the cave)*/);
+  translate(drawX + 600, drawY + 350);
+  drawPlantType1();
+  pop();
+  //plant type 2 - placement 1
+  drawPlantType2();
+  push(/*plant type 2 - placement 2*/);
+  translate(drawX + 50, drawY);
+  drawPlantType2();
+  pop();
+
+  //DRAWING THE WATER VARIATION/REFLECTION
+  drawWaterVariation();
 }
